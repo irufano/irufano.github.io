adbc896ce6052deb62f0277cb425cce25a18035a
irufano <irufano@users.noreply.github.com>
2026-09-06 10:48:06 +0000

    deploy: bd8fad99ab4dd6a0ff31f415b2340eeca9fb13b2

---
+++ _app/immutable/chunks/C5c6IISQ.js
@@ -0,0 +1,1 @@
+import"./DZKW0ZOJ.js";function e(e){return e.toLowerCase().trim().replace(/[^a-z0-9]+/g,`-`).replace(/(^-+|-+$)/g,``)}export{e as t};
---
+++ _app/immutable/entry/start.Cdwx9OQX.js
@@ -0,0 +1,1 @@
+import{n as e,s as t}from"../chunks/DZKW0ZOJ.js";export{t as load_css,e as start};
